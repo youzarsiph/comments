c6dcb92bfe1bb84525f8ccbb456725c6fb8ce914
Yousef Abu Shanab <josephyousef249@gmail.com>
2023-02-20 14:11:28 +0200

    Initial Setup

---
+++ apps.py
@@ -0,0 +1,11 @@
+""" AppConf """
+
+
+from django.apps import AppConfig
+
+
+class CommentsConfig(AppConfig):
+    """ App Cong=figuration for comments """
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'comments'
